30403cf484d2d64828079e72f11323de4e2878c3
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-03-16 13:21:44 +0100

    Created TopNav component

--- src/components/screens/Home.vue
+++ src/components/screens/Home.vue
@@ -1,10 +1,18 @@
 <template>
-    <h1>Home</h1>
+    <div>
+        <top-nav></top-nav>
+        <h1>Home</h1>
+    </div>
 </template>
 
 
 <script>
+import TopNav from '../ui/TopNav'
+
 export default {
     name: 'Home',
+    components: {
+        TopNav,
+    }
 }
 </script>
