d427a7679b63a557942b58ff7c32c715a5bc7bd9
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-03-17 16:26:03 +0100

    Added slider in menu

--- src/components/ui/MenuSlider.vue
+++ src/components/ui/MenuSlider.vue
@@ -38,16 +38,160 @@
                 </div>
             </md-tab>
             <md-tab id="tab-day2" md-label="MAR 3">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis nihil vel beatae a obcaecati dolore quasi, odio veritatis ab, facilis quo cupiditate sapiente deleniti sequi quidem aperiam eum qui iusto.</p>
+                <h1>Faux filet et sa sauce au vin rouge</h1>
+                <div class="image" style="background-image: url('https://images.unsplash.com/photo-1504973960431-1c467e159aa4?ixlib=rb-0.3.5&s=2195ba5081c67a1865b0d2c284d43853&auto=format&fit=crop&w=1350&q=80');">
+                    <!-- <img src= alt=""> -->
+                </div>
+                <div class="group">
+                    <div class="addToKart">
+                        <span class="price">15€</span>
+                        <button class="addKart">
+                            + Ajouter au panier
+                        </button>
+                    </div>
+                    <md-tabs>
+                        <md-tab id="plats" md-label="plats">
+                            <ul>
+                                <li>Entrée</li>
+                                <li>Potage vert</li>
+                                <li>Plat</li>
+                                <li>Faux filet et sa sauce au vin rouge</li>
+                                <li>Dessert</li>
+                                <li>Crème au chocolat</li>
+                            </ul>
+                        </md-tab>
+                        <md-tab id="ingredient" md-label="ingredient">
+                            <ul>
+                                <li>Entrée</li>
+                                <li>oignon, ail, huile d'olive, bouillon de poulet, brocoli, asperge, épinard, sel, poivre, fromage feta</li>
+                                <li>Plat</li>
+                                <li>faux-filet de boeuf, oignon rouge, thym, vin rouge, eau, beurre, sel, poivre</li>
+                                <li>Dessert</li>
+                                <li>chocolat noir, jaune d'oeuf, blanc d'oeuf, lait, sucre vanillé, sucre, maïzena, crème liquide, chocolat</li>
+                            </ul>
+                        </md-tab>
+                    </md-tabs>
+                </div>
             </md-tab>
             <md-tab id="tab-day3" md-label="MER 4">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis nihil vel beatae a obcaecati dolore quasi, odio veritatis ab, facilis quo cupiditate sapiente deleniti sequi quidem aperiam eum qui iusto.</p>
+                <h1>Faux filet et sa sauce au vin rouge</h1>
+                <div class="image" style="background-image: url('https://images.unsplash.com/photo-1504973960431-1c467e159aa4?ixlib=rb-0.3.5&s=2195ba5081c67a1865b0d2c284d43853&auto=format&fit=crop&w=1350&q=80');">
+                    <!-- <img src= alt=""> -->
+                </div>
+                <div class="group">
+                    <div class="addToKart">
+                        <span class="price">15€</span>
+                        <button class="addKart">
+                            + Ajouter au panier
+                        </button>
+                    </div>
+                    <md-tabs>
+                        <md-tab id="plats" md-label="plats">
+                            <ul>
+                                <li>Entrée</li>
+                                <li>Potage vert</li>
+                                <li>Plat</li>
+                                <li>Faux filet et sa sauce au vin rouge</li>
+                                <li>Dessert</li>
+                                <li>Crème au chocolat</li>
+                            </ul>
+                        </md-tab>
+                        <md-tab id="ingredient" md-label="ingredient">
+                            <ul>
+                                <li>Entrée</li>
+                                <li>oignon, ail, huile d'olive, bouillon de poulet, brocoli, asperge, épinard, sel, poivre, fromage feta</li>
+                                <li>Plat</li>
+                                <li>faux-filet de boeuf, oignon rouge, thym, vin rouge, eau, beurre, sel, poivre</li>
+                                <li>Dessert</li>
+                                <li>chocolat noir, jaune d'oeuf, blanc d'oeuf, lait, sucre vanillé, sucre, maïzena, crème liquide, chocolat</li>
+                            </ul>
+                        </md-tab>
+                    </md-tabs>
+                </div>
             </md-tab>
             <md-tab id="tab-day4" md-label="JEU 5">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis nihil vel beatae a obcaecati dolore quasi, odio veritatis ab, facilis quo cupiditate sapiente deleniti sequi quidem aperiam eum qui iusto.</p>
+                <h1>Faux filet et sa sauce au vin rouge</h1>
+                <div class="image" style="background-image: url('https://images.unsplash.com/photo-1504973960431-1c467e159aa4?ixlib=rb-0.3.5&s=2195ba5081c67a1865b0d2c284d43853&auto=format&fit=crop&w=1350&q=80');">
+                    <!-- <img src= alt=""> -->
+                </div>
+                <div class="group">
+                    <div class="addToKart">
+                        <span class="price">15€</span>
+                        <button class="addKart">
+                            + Ajouter au panier
+                        </button>
+                    </div>
+                    <md-tabs>
+                        <md-tab id="plats" md-label="plats">
+                            <ul>
+                                <li>Entrée</li>
+                                <li>Potage vert</li>
+                                <li>Plat</li>
+                                <li>Faux filet et sa sauce au vin rouge</li>
+                                <li>Dessert</li>
+                                <li>Crème au chocolat</li>
+                            </ul>
+                        </md-tab>
+                        <md-tab id="ingredient" md-label="ingredient">
+                            <ul>
+                                <li>Entrée</li>
+                                <li>oignon, ail, huile d'olive, bouillon de poulet, brocoli, asperge, épinard, sel, poivre, fromage feta</li>
+                                <li>Plat</li>
+                                <li>faux-filet de boeuf, oignon rouge, thym, vin rouge, eau, beurre, sel, poivre</li>
+                                <li>Dessert</li>
+                                <li>chocolat noir, jaune d'oeuf, blanc d'oeuf, lait, sucre vanillé, sucre, maïzena, crème liquide, chocolat</li>
+                            </ul>
+                        </md-tab>
+                    </md-tabs>
+                </div>
             </md-tab>
             <md-tab id="tab-day5" md-label="VEN 6">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis nihil vel beatae a obcaecati dolore quasi, odio veritatis ab, facilis quo cupiditate sapiente deleniti sequi quidem aperiam eum qui iusto.</p>
+                <h1>Faux filet et sa sauce au vin rouge</h1>
+                <div class="image">
+                        <swiper :options="swiperOptionTop" class="gallery-top" ref="swiperTop">
+                            <swiper-slide class="slide-1"></swiper-slide>
+                            <swiper-slide class="slide-2"></swiper-slide>
+                            <swiper-slide class="slide-3"></swiper-slide>
+                            <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+                            <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
+                        </swiper>
+                        <!-- swiper2 Thumbs -->
+                        <swiper :options="swiperOptionThumbs" class="gallery-thumbs" ref="swiperThumbs">
+                            <swiper-slide class="slide-1"></swiper-slide>
+                            <swiper-slide class="slide-2"></swiper-slide>
+                            <swiper-slide class="slide-3"></swiper-slide>
+                        </swiper>
+                </div>
+                <div class="group">
+                    <div class="addToKart">
+                        <span class="price">15€</span>
+                        <button class="addKart">
+                            + Ajouter au panier
+                        </button>
+                    </div>
+                    <md-tabs>
+                        <md-tab id="plats" md-label="plats">
+                            <ul>
+                                <li>Entrée</li>
+                                <li>Potage vert</li>
+                                <li>Plat</li>
+                                <li>Faux filet et sa sauce au vin rouge</li>
+                                <li>Dessert</li>
+                                <li>Crème au chocolat</li>
+                            </ul>
+                        </md-tab>
+                        <md-tab id="ingredient" md-label="ingredient">
+                            <ul>
+                                <li>Entrée</li>
+                                <li>oignon, ail, huile d'olive, bouillon de poulet, brocoli, asperge, épinard, sel, poivre, fromage feta</li>
+                                <li>Plat</li>
+                                <li>faux-filet de boeuf, oignon rouge, thym, vin rouge, eau, beurre, sel, poivre</li>
+                                <li>Dessert</li>
+                                <li>chocolat noir, jaune d'oeuf, blanc d'oeuf, lait, sucre vanillé, sucre, maïzena, crème liquide, chocolat</li>
+                            </ul>
+                        </md-tab>
+                    </md-tabs>
+                </div>
             </md-tab>
         </md-tabs>
         <complete-menu></complete-menu>
@@ -57,12 +201,79 @@
 <script>
 import CompleteMenu from './CompleteMenu'
 
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
 export default {  
     name: 'MenuSlider',
     components: {
-        CompleteMenu,
+        CompleteMenu, swiper, swiperSlide
+    },
+    
+    data() {
+      return {
+        swiperOptionTop: {
+          spaceBetween: 10,
+          loop: true,
+          loopedSlides: 5, //looped slides should be the same
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          }
+        },
+        swiperOptionThumbs: {
+          spaceBetween: 10,
+          slidesPerView: 4,
+          touchRatio: 0.2,
+          loop: true,
+          loopedSlides: 5, //looped slides should be the same
+          slideToClickedSlide: true,
+        }
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+        const swiperTop = this.$refs.swiperTop.swiper
+        const swiperThumbs = this.$refs.swiperThumbs.swiper
+        swiperTop.controller.control = swiperThumbs
+        swiperThumbs.controller.control = swiperTop
+      })
     }
 }
 </script>
 
 
+<style lang="scss" scoped>
+  .swiper-container {
+    background-color: #000;
+  }
+  .swiper-slide {
+    background-size: cover;
+    background-position: center;
+    &.slide-1 {
+      background-image:url('https://images.unsplash.com/photo-1474898856510-884a2c0be546?ixlib=rb-0.3.5&s=8a79a6c04ef1aa308d31a13646a4c8e5&auto=format&fit=crop&w=1267&q=80');
+    }
+    &.slide-2 {
+      background-image:url('https://images.unsplash.com/photo-1513267048331-5611cad62e41?ixlib=rb-0.3.5&s=06a5f840ce65add98e535354489598b9&auto=format&fit=crop&w=1350&q=80');
+    }
+    &.slide-3 {
+      background-image:url('https://images.unsplash.com/photo-1507010228826-fd02d8c83ddf?ixlib=rb-0.3.5&s=f2300fd0e920a202a946f248f487a482&auto=format&fit=crop&w=1341&q=80');
+    }
+  }
+  .gallery-top {
+    height: 80%!important;
+    width: 100%;
+  }
+  .gallery-thumbs {
+    height: 20%!important;
+    box-sizing: border-box;
+    padding: 10px 0;
+  }
+  .gallery-thumbs .swiper-slide {
+    width: 25%;
+    height: 100%;
+    opacity: 0.4;
+  }
+  .gallery-thumbs .swiper-slide-active {
+    opacity: 1;
+  }
+</style>
