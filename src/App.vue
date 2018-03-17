85cd6ec9c9dc532dee5739ee090bb30a4b2bc5ab
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-03-17 13:52:04 +0100

    Created and designed footer

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <top-nav></top-nav>
     <router-view/>
+    <footer-item></footer-item>
   </div>
 </template>
 
 <script>
 import TopNav from './components/ui/TopNav'
+import FooterItem from './components/ui/Footer'
 export default {
   name: 'App',
   components: {
     TopNav,
+    FooterItem
   }
 }
 </script>
