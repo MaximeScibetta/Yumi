4fb12679923b25429905c49d5da8825321efc0a8
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-03-18 13:33:53 +0100

    Created about component

---
+++ src/components/screens/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="about">
+        <div class="about__content"></div>
+        <div class="about__image"></div>
+        <div class="about__content"></div>
+        <div class="about__image"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About',
+} 
+</script>
