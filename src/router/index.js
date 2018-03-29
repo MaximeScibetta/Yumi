ff3646f71542314216435bf641ab7d752d01b5b2
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-03-29 09:38:05 +0200

    Fixed scroll on routing

--- src/router/index.js
+++ src/router/index.js
@@ -32,5 +32,15 @@ export default new Router({
       component: Faq
     },
 
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return {
+        x: 0,
+        y: 0
+      }
+    }
+  }
 })
